0268eae95f34fdd3624675e5917bd0a4f8545593
Willyan Paproski Bueno <94638288+willyanpaproski@users.noreply.github.com>
2025-06-05 23:57:14 -0300

    Delete FrontEnd/front-end directory

--- FrontEnd/front-end/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-
-export default function ProtectedRoute({ children }) {
-    const { token } = useAuth();
-
-    return token ? children : <Navigate to="/login" />;
-}
